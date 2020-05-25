cc1c0d087a47ceff9ea2115bca9a8865ffebcc99
tam nguyen <ndtam.273@gmail.com>
2020-05-25 22:32:16 +0700

    chuong 12

--- todolist/AddItemViewController.swift
+++ todolist/AddItemViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +35,24 @@ class AddItemViewController: UITableViewController {
     // MARK:- Table View Delegates
     override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
         return nil
+    }
+    
+    // MARK:- TextField Delegates
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        print(oldText)
+        let stringRange = Range(range, in: oldText)!
+        print(stringRange)
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        print(newText)
+        doneBarButton.isEnabled = !newText.isEmpty
+        return true
+        
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
     }
 
 }
